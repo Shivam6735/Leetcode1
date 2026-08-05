32a57c88872f3daf66537dd34cf0c46521d53c27
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-05 21:09:17 +0530

    Time: 156 ms (88.97%) | Memory: 298 MB (87.93%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -2,38 +2,38 @@ class Solution {
 public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
         vector<vector<int>> g(n);
-
         for (auto &e : invocations)
             g[e[0]].push_back(e[1]);
 
-        vector<int> suspicious(n, 0);
+        vector<bool> vis(n, false);
+        queue<int> q;
+        q.push(k);
+        vis[k] = true;
 
-        function<void(int)> dfs = [&](int u) {
-            suspicious[u] = 1;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
             for (int v : g[u]) {
-                if (!suspicious[v])
-                    dfs(v);
+                if (!vis[v]) {
+                    vis[v] = true;
+                    q.push(v);
+                }
             }
-        };
+        }
 
-        dfs(k);
-
-        // Check whether any outside node calls a suspicious node
         for (auto &e : invocations) {
-            int u = e[0], v = e[1];
-            if (!suspicious[u] && suspicious[v]) {
-                vector<int> ans;
-                for (int i = 0; i < n; i++)
-                    ans.push_back(i);
+            if (!vis[e[0]] && vis[e[1]]) {
+                vector<int> ans(n);
+                iota(ans.begin(), ans.end(), 0);
                 return ans;
             }
         }
 
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            if (!suspicious[i])
-                ans.push_back(i);
-        }
+        for (int i = 0; i < n; i++)
+            if (!vis[i]) ans.push_back(i);
+
         return ans;
     }
 };
