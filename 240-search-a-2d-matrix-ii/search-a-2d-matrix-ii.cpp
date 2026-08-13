ca93f8444130c8a3c0791a42cce57309aa1a9882
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-13 05:47:48 +0530

    Time: 51 ms (69.61%) | Memory: 18.8 MB (39.83%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,15 +1,44 @@
 class Solution {
 public:
+    // bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     int rows = matrix.size();
+    //     int cols = matrix[0].size();
+    //     for(int i=0; i<rows; i++){
+    //         for(int j=0; j<cols; j++){
+    //             if(matrix[i][j] == target){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty()){
+            return false;
+        }
+
         int rows = matrix.size();
         int cols = matrix[0].size();
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+
+        int row = 0;
+        int col = cols-1;
+
+        while(row < rows && col >=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
             }
         }
+        
         return false;
     }
 };
