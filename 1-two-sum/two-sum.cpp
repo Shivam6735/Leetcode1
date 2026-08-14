2004c870200c02a7e1ac0997a7250d2cb331cd20
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-14 06:12:19 +0530

    Time: 4 ms (51.70%) | Memory: 14.8 MB (41.68%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,61 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n= nums.size();
-        vector<int> ans;
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     int n= nums.size();
+    //     vector<int> ans;
 
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++)
-            if(nums[i]+nums[j] == target){
-                ans.push_back(i);
-                ans.push_back(j);
+    //     for(int i=0; i<n-1; i++){
+    //         for(int j=i+1; j<n; j++)
+    //         if(nums[i]+nums[j] == target){
+    //             ans.push_back(i);
+    //             ans.push_back(j);
 
-                return ans;
-            }
-        }
-        return ans;
+    //             return ans;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+
+
+
+
+    // Better Approach
+
+   vector<int> twoSum(vector<int>& nums, int target) {
+    int n = nums.size();
+
+    // Store {value, original index}
+    vector<pair<int, int>> arr;
+
+    for(int i = 0; i < n; i++) {
+        arr.push_back({nums[i], i});
     }
+
+    // Sort according to value
+    sort(arr.begin(), arr.end());
+
+    int st = 0;
+    int end = n - 1;
+
+    while(st < end) {
+
+        int sum = arr[st].first + arr[end].first;
+
+        if(sum == target) {
+            return {arr[st].second, arr[end].second};
+        }
+
+        else if(sum < target) {
+            st++;
+        }
+
+        else {
+            end--;
+        }
+    }
+
+    return {};
+}
 };
