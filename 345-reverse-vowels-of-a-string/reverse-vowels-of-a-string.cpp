277a4b65f61017b60ece26ae9152fd772103250a
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-16 03:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (32.22%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -55,6 +55,53 @@
 
 // BETTER APPROACH
 
+// class Solution {
+// public:
+//     bool isVowel(char c) {
+//         return c == 'a' || c == 'e' || c == 'i' ||
+//                c == 'o' || c == 'u' ||
+//                c == 'A' || c == 'E' || c == 'I' ||
+//                c == 'O' || c == 'U';
+//     }
+
+//     string reverseVowels(string s) {
+//         vector<int> pos;
+
+//         for (int i = 0; i < s.size(); i++) {
+//             if (isVowel(s[i]))
+//                 pos.push_back(i);
+//         }
+
+//         int left = 0;
+//         int right = pos.size() - 1;
+
+//         while (left < right) {
+//             swap(s[pos[left]], s[pos[right]]);
+//             left++;
+//             right--;
+//         }
+
+//         return s;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Most Optimal using Two Pointer
+
 class Solution {
 public:
     bool isVowel(char c) {
@@ -65,18 +112,24 @@ public:
     }
 
     string reverseVowels(string s) {
-        vector<int> pos;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i]))
-                pos.push_back(i);
-        }
-
         int left = 0;
-        int right = pos.size() - 1;
+        int right = s.size() - 1;
 
         while (left < right) {
-            swap(s[pos[left]], s[pos[right]]);
+
+            // Find vowel from left
+            while (left < right && !isVowel(s[left])) {
+                left++;
+            }
+
+            // Find vowel from right
+            while (left < right && !isVowel(s[right])) {
+                right--;
+            }
+
+            // Swap vowels
+            swap(s[left], s[right]);
+
             left++;
             right--;
         }
@@ -84,4 +137,5 @@ public:
         return s;
     }
 };
+
 
