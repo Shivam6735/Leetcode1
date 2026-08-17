012c74b5c07722e93efd60d2a5bf925dd2a09b60
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-18 05:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.08%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+
+        for (char stone : stones) {
+            for (char jewel : jewels) {
+                if (stone == jewel) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+};
