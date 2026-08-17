e0107e7e4ca3530cf9f72084e3e72ebfb60d5a73
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-18 05:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (42.65%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,17 +1,51 @@
+// class Solution {
+// public:
+//     int numJewelsInStones(string jewels, string stones) {
+//         int count = 0;
+
+//         for (char stone : stones) {
+//             for (char jewel : jewels) {
+//                 if (stone == jewel) {
+//                     count++;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> st;
+
+        for (char c : jewels) {
+            st.insert(c);
+        }
+
         int count = 0;
 
-        for (char stone : stones) {
-            for (char jewel : jewels) {
-                if (stone == jewel) {
-                    count++;
-                    break;
-                }
+        for (char c : stones) {
+            if (st.count(c)) {
+                count++;
             }
         }
 
         return count;
     }
 };
+
