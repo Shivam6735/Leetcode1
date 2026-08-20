938e071055aedfbcab7ba7d02d0053dd342b4bf7
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-20 22:15:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (66.37%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -1,26 +1,56 @@
+// class Solution {
+// public:
+//     vector<int> resultArray(vector<int>& nums) {
+//         int n = nums.size();
+
+//         vector<int> arr1 = {nums[0]};
+//         vector<int> arr2 = {nums[1]};
+//         vector<int> ans;
+//         for(int i=2; i<n; i++){
+
+
+//             if(arr1.back() > arr2.back()){
+//                 arr1.push_back(nums[i]);
+//             } else{
+//                 arr2.push_back(nums[i]);
+//             }
+
+            
+//         }
+//         ans.insert(ans.end(),arr1.begin(), arr1.end());
+//             ans.insert(ans.end(),arr2.begin(), arr2.end());
+//             return ans;
+        
+
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
-        int n = nums.size();
 
         vector<int> arr1 = {nums[0]};
         vector<int> arr2 = {nums[1]};
-        vector<int> ans;
-        for(int i=2; i<n; i++){
 
+        for(int i = 2; i < nums.size(); i++) {
 
-            if(arr1.back() > arr2.back()){
+            if(arr1.back() > arr2.back())
                 arr1.push_back(nums[i]);
-            } else{
+            else
                 arr2.push_back(nums[i]);
-            }
-
-            
         }
-        ans.insert(ans.end(),arr1.begin(), arr1.end());
-            ans.insert(ans.end(),arr2.begin(), arr2.end());
-            return ans;
-        
 
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+
+        return arr1;
     }
 };
