6957ad96e9ea98434699496b367a5a443e0477c5
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-25 05:22:47 +0530

    Time: 2 ms (5.14%) | Memory: 8.5 MB (79.77%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,17 +1,47 @@
+// class Solution {
+// public:
+//     int addDigits(int num) {
+//         while (num >= 10) {
+//             int sum = 0;
+
+//             while (num > 0) {
+//                 sum += num % 10;
+//                 num /= 10;
+//             }
+
+//             num = sum;
+//         }
+
+//         return num;
+//     }
+// };
+
+
+
+
+
+
+
+
 class Solution {
 public:
+    int digitSum(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
     int addDigits(int num) {
         while (num >= 10) {
-            int sum = 0;
-
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
-            }
-
-            num = sum;
+            num = digitSum(num);
         }
 
         return num;
     }
 };
+
