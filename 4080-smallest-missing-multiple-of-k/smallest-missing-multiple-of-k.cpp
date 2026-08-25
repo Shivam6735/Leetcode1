2d71c67cfee240b2d9c6397c4caa27ad308955a1
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-26 02:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (99.68%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -28,18 +28,60 @@
 
 
 
+// class Solution {
+// public:
+//     int missingMultiple(vector<int>& nums, int k) {
+//         unordered_set<int> st(nums.begin(), nums.end());
+
+//         int multiple = k;
+
+//         while (st.count(multiple)) {
+//             multiple += k;
+//         }
+
+//         return multiple;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> st(nums.begin(), nums.end());
+        int n = nums.size();
 
-        int multiple = k;
+        vector<bool> present(n + 1, false);
 
-        while (st.count(multiple)) {
-            multiple += k;
+        for (int x : nums) {
+            if (x % k == 0) {
+                int index = x / k;
+
+                if (index <= n)
+                    present[index] = true;
+            }
         }
 
-        return multiple;
+        for (int i = 1; i <= n + 1; i++) {
+            if (!present[i])
+                return i * k;
+        }
+
+        return -1;
     }
 };
 
