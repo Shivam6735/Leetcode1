892cd7593b1660ff89dd9fef2e4ef4d47461c500
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-26 02:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (24.24%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     int missingMultiple(vector<int>& nums, int k) {
+//         int multiple = k;
+
+//         while (true) {
+//             bool found = false;
+
+//             for (int x : nums) {
+//                 if (x == multiple) {
+//                     found = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!found)
+//                 return multiple;
+
+//             multiple += k;
+//         }
+//     }
+// };
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
         int multiple = k;
 
-        while (true) {
-            bool found = false;
-
-            for (int x : nums) {
-                if (x == multiple) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found)
-                return multiple;
-
+        while (st.count(multiple)) {
             multiple += k;
         }
+
+        return multiple;
     }
 };
+
