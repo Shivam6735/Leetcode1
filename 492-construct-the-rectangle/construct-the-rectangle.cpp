36bda890339cd7df851c0ab76e93667ffdcae5df
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-08-28 05:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.07%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -1,20 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> constructRectangle(int area) {
+//         int bestL = area, bestW = 1;
+
+//         for (int W = 1; W * W <= area; W++) {
+//             if (area % W == 0) {
+//                 int L = area / W;
+
+//                 if (L - W < bestL - bestW) {
+//                     bestL = L;
+//                     bestW = W;
+//                 }
+//             }
+//         }
+
+//         return {bestL, bestW};
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        int bestL = area, bestW = 1;
+        int W = sqrt(area);
 
-        for (int W = 1; W * W <= area; W++) {
-            if (area % W == 0) {
-                int L = area / W;
-
-                if (L - W < bestL - bestW) {
-                    bestL = L;
-                    bestW = W;
-                }
-            }
+        while (area % W != 0) {
+            W--;
         }
 
-        return {bestL, bestW};
+        return {area / W, W};
     }
 };
+
 
