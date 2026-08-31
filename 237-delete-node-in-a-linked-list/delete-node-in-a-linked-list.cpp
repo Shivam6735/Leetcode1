6025e2c139df13c4c922e80b19530ef757449fa2
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-01 02:14:00 +0530

    Time: 5 ms (78.46%) | Memory: 12.5 MB (22.80%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -6,18 +6,41 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     void deleteNode(ListNode* node) {
+//         ListNode* prev  = NULL;
+        
+//         while(node != NULL && node->next != NULL){
+//             node->val = node->next->val;
+
+//             prev = node;  // assigning to the previous
+//             node = node->next;
+//         }
+//         prev->next = NULL;
+//         delete(node);
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// KHUD SE
+
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* prev  = NULL;
-        
-        while(node != NULL && node->next != NULL){
-            node->val = node->next->val;
-
-            prev = node;  // assigning to the previous
-            node = node->next;
-        }
-        prev->next = NULL;
-        delete(node);
+       node->val = node->next->val;
+       node->next = node->next->next;
     }
 };
