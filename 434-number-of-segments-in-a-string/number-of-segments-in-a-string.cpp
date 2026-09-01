99a132039f9a52a6c3d0d48bdd46ecf6cd77f717
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-02 01:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (21.33%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -23,12 +23,39 @@
 
 
 
+// class Solution {
+// public:
+//     int countSegments(string s) {
+//         int count = 0;
+
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int countSegments(string s) {
         int count = 0;
 
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
                 count++;
             }
@@ -37,4 +64,5 @@ public:
         return count;
     }
 };
+
 
