b33353d9e52ddf757437500c8af70a56cefddc6c
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-04 10:06:05 +0530

    Time: 6 ms (91.93%) | Memory: 11.7 MB (97.34%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -48,24 +48,55 @@
 
 // BETTER APPROACH USING HASHSET
 
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         unordered_set<ListNode*> visited;
+
+//         ListNode* temp = head;
+//         while(temp != NULL){
+//             if(visited.count(temp)){
+//                 return true;
+//             }
+//             else {
+//                 visited.insert(temp);
+//                 temp = temp->next;
+//             }
+            
+//         }
+//         return false;
+//     }
+
+
+// };
+
+
+
+
+
+
+
+
+
+// BEST Optimal Solution
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*> visited;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // ListNode* temp = head;
 
-        ListNode* temp = head;
-        while(temp != NULL){
-            if(visited.count(temp)){
-                return true;
-            }
-            else {
-                visited.insert(temp);
-                temp = temp->next;
-            }
-            
+    while(fast != NULL && fast->next != NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if(fast == slow){
+            return true;
         }
-        return false;
+        
     }
-
+    return false;
+    }
 
 };
