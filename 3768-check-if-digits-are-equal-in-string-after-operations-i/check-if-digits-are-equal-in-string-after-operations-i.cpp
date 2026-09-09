bd1c648fa443f50034d492e0f713d52455ed13a7
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-09 05:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.53%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,19 +1,51 @@
+// class Solution {
+// public:
+//     bool hasSameDigits(string s) {
+//         while (s.size() > 2) {
+//             string next = "";
+
+//             for (int i = 0; i < s.size() - 1; i++) {
+//                 int a = s[i] - '0';
+//                 int b = s[i + 1] - '0';
+
+//                 next += char('0' + (a + b) % 10);
+//             }
+
+//             s = next;
+//         }
+
+//         return s[0] == s[1];
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        while (s.size() > 2) {
-            string next = "";
+        int n = s.size();
 
-            for (int i = 0; i < s.size() - 1; i++) {
+        for (int len = n; len > 2; len--) {
+            for (int i = 0; i < len - 1; i++) {
                 int a = s[i] - '0';
                 int b = s[i + 1] - '0';
 
-                next += char('0' + (a + b) % 10);
+                s[i] = char('0' + (a + b) % 10);
             }
-
-            s = next;
         }
 
         return s[0] == s[1];
     }
 };
+
