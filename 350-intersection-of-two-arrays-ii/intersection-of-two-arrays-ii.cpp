a1e6c2d7371d66edd19d6893a1ca900bdc1f0bcd
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-16 00:30:14 +0530

    Time: 4 ms (15.98%) | Memory: 14.7 MB (42.46%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -45,28 +45,63 @@
 
 
 
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+//         sort(nums1.begin(), nums1.end());
+//         sort(nums2.begin(), nums2.end());
+
+//         vector<int> ans;
+
+//         int i = 0;
+//         int j = 0;
+
+//         while (i < nums1.size() && j < nums2.size()) {
+//             if (nums1[i] == nums2[j]) {
+//                 ans.push_back(nums1[i]);
+//                 i++;
+//                 j++;
+//             }
+//             else if (nums1[i] < nums2[j]) {
+//                 i++;
+//             }
+//             else {
+//                 j++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-
+        unordered_map<int, int> freq;
         vector<int> ans;
 
-        int i = 0;
-        int j = 0;
+        for (int num : nums1) {
+            freq[num]++;
+        }
 
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] == nums2[j]) {
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if (nums1[i] < nums2[j]) {
-                i++;
-            }
-            else {
-                j++;
+        for (int num : nums2) {
+            if (freq[num] > 0) {
+                ans.push_back(num);
+                freq[num]--;
             }
         }
 
