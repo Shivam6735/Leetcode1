86c9efe69abb658a0e7778ad1b7de6109a971628
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-16 00:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (62.65%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -14,10 +14,9 @@
 //             }
 //         }
 
-
-
-
-
+//         return ans;
+//     }
+// };
 
 
 
@@ -49,24 +48,28 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> freq;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
         vector<int> ans;
 
-        for (int num : nums1) {
-            freq[num]++;
-        }
+        int i = 0;
+        int j = 0;
 
-        for (int num : nums2) {
-            if (freq[num] > 0) {
-                ans.push_back(num);
-                freq[num]--;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
             }
         }
 
         return ans;
     }
 };
-
-//         return ans;
-//     }
-// };
