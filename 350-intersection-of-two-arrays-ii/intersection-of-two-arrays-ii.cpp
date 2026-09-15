1b552cf5f6265ee4dc9b33ad1f7cc5a570464f8c
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-16 00:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (31.23%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,19 +1,72 @@
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> ans;
+//         vector<bool> used(nums2.size(), false);
+
+//         for (int i = 0; i < nums1.size(); i++) {
+//             for (int j = 0; j < nums2.size(); j++) {
+//                 if (nums1[i] == nums2[j] && !used[j]) {
+//                     ans.push_back(nums1[i]);
+//                     used[j] = true;
+//                     break;
+//                 }
+//             }
+//         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> freq;
         vector<int> ans;
-        vector<bool> used(nums2.size(), false);
 
-        for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] == nums2[j] && !used[j]) {
-                    ans.push_back(nums1[i]);
-                    used[j] = true;
-                    break;
-                }
+        for (int num : nums1) {
+            freq[num]++;
+        }
+
+        for (int num : nums2) {
+            if (freq[num] > 0) {
+                ans.push_back(num);
+                freq[num]--;
             }
         }
 
         return ans;
     }
 };
+
+//         return ans;
+//     }
+// };
