9f1b412fa1ba3378f0cb13bb1596b32ee70ca406
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-19 00:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (78.93%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,25 +1,59 @@
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+//             int leftSum = 0;
+//             int rightSum = 0;
+
+//             // Sum of elements on the left
+//             for (int j = 0; j < i; j++) {
+//                 leftSum += nums[j];
+//             }
+
+//             // Sum of elements on the right
+//             for (int j = i + 1; j < n; j++) {
+//                 rightSum += nums[j];
+//             }
+
+//             if (leftSum == rightSum) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
+        int totalSum = 0;
 
-        for (int i = 0; i < n; i++) {
-            int leftSum = 0;
-            int rightSum = 0;
+        for (int x : nums) {
+            totalSum += x;
+        }
 
-            // Sum of elements on the left
-            for (int j = 0; j < i; j++) {
-                leftSum += nums[j];
-            }
+        int leftSum = 0;
 
-            // Sum of elements on the right
-            for (int j = i + 1; j < n; j++) {
-                rightSum += nums[j];
-            }
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
 
             if (leftSum == rightSum) {
                 return i;
             }
+
+            leftSum += nums[i];
         }
 
         return -1;
