ed0c141e94b4190c173c42fe9ec22a8400f015ac
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-20 01:56:01 +0530

    Time: 2 ms (83.68%) | Memory: 15.6 MB (64.14%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,28 +1,73 @@
+// class Solution {
+// public:
+//     vector<string> findRelativeRanks(vector<int>& score) {
+//         int n = score.size();
+//         vector<string> ans(n);
+
+//         for (int i = 0; i < n; i++) {
+//             int rank = 1;
+
+//             for (int j = 0; j < n; j++) {
+//                 if (score[j] > score[i]) {
+//                     rank++;
+//                 }
+//             }
+
+//             if (rank == 1)
+//                 ans[i] = "Gold Medal";
+//             else if (rank == 2)
+//                 ans[i] = "Silver Medal";
+//             else if (rank == 3)
+//                 ans[i] = "Bronze Medal";
+//             else
+//                 ans[i] = to_string(rank);
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
-        vector<string> ans(n);
+
+        vector<pair<int, int>> arr;
 
         for (int i = 0; i < n; i++) {
-            int rank = 1;
+            arr.push_back({score[i], i});
+        }
 
-            for (int j = 0; j < n; j++) {
-                if (score[j] > score[i]) {
-                    rank++;
-                }
-            }
+        sort(arr.begin(), arr.end(), greater<pair<int, int>>());
 
-            if (rank == 1)
-                ans[i] = "Gold Medal";
+        vector<string> ans(n);
+
+        for (int rank = 0; rank < n; rank++) {
+            int originalIndex = arr[rank].second;
+
+            if (rank == 0)
+                ans[originalIndex] = "Gold Medal";
+            else if (rank == 1)
+                ans[originalIndex] = "Silver Medal";
             else if (rank == 2)
-                ans[i] = "Silver Medal";
-            else if (rank == 3)
-                ans[i] = "Bronze Medal";
+                ans[originalIndex] = "Bronze Medal";
             else
-                ans[i] = to_string(rank);
+                ans[originalIndex] = to_string(rank + 1);
         }
 
         return ans;
     }
 };
+
