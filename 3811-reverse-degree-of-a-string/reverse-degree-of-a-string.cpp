7469ebf614b9ed618b3128738e6e27f374eaccd7
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-21 03:53:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.66%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -33,15 +33,41 @@
 
 
 
+// class Solution {
+// public:
+//     int reverseDegree(string s) {
+//         int ans = 0;
+
+//         for (int i = 0; i < s.size(); i++) {
+//             int reverseValue = 26 - (s[i] - 'a');
+
+//             ans += (i + 1) * reverseValue;
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int reverseDegree(string s) {
         int ans = 0;
 
         for (int i = 0; i < s.size(); i++) {
-            int reverseValue = 26 - (s[i] - 'a');
-
-            ans += (i + 1) * reverseValue;
+            ans += (i + 1) * ('z' - s[i] + 1);
         }
 
         return ans;
