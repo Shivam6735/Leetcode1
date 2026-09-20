ad0abc1aa297c987f74ab1a3f36592ce0bab70a3
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-21 03:53:01 +0530

    Time: 4 ms (9.46%) | Memory: 9.6 MB (96.13%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,18 +1,45 @@
+// class Solution {
+// public:
+//     int reverseDegree(string s) {
+//         int ans = 0;
+
+//         for (int i = 0; i < s.size(); i++) {
+//             int reverseValue = 0;
+
+//             // Search from z to find the reverse position
+//             for (char c = 'z'; c >= 'a'; c--) {
+//                 reverseValue++;
+
+//                 if (c == s[i])
+//                     break;
+//             }
+
+//             ans += (i + 1) * reverseValue;
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int reverseDegree(string s) {
         int ans = 0;
 
         for (int i = 0; i < s.size(); i++) {
-            int reverseValue = 0;
-
-            // Search from z to find the reverse position
-            for (char c = 'z'; c >= 'a'; c--) {
-                reverseValue++;
-
-                if (c == s[i])
-                    break;
-            }
+            int reverseValue = 26 - (s[i] - 'a');
 
             ans += (i + 1) * reverseValue;
         }
@@ -20,3 +47,4 @@ public:
         return ans;
     }
 };
+
