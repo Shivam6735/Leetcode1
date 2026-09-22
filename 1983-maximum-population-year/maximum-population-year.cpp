ea18a4a5fd878959399b1dd32b48c36961f2edfe
Shivam Kumar Shukla <153048193+Shivam6735@users.noreply.github.com>
2026-09-23 00:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (78.06%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -35,26 +35,86 @@
 
 
 
+// class Solution {
+// public:
+//     int maximumPopulation(vector<vector<int>>& logs) {
+//         int population[101] = {};
+
+//         for (auto& person : logs) {
+//             int birth = person[0];
+//             int death = person[1];
+
+//             for (int year = birth; year < death; year++) {
+//                 population[year - 1950]++;
+//             }
+//         }
+
+//         int maxPop = 0;
+//         int ans = 1950;
+
+//         for (int i = 0; i <= 100; i++) {
+//             if (population[i] > maxPop) {
+//                 maxPop = population[i];
+//                 ans = 1950 + i;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        int population[101] = {};
+        int diff[101] = {};
 
         for (auto& person : logs) {
             int birth = person[0];
             int death = person[1];
 
-            for (int year = birth; year < death; year++) {
-                population[year - 1950]++;
-            }
+            diff[birth - 1950]++;
+            diff[death - 1950]--;
         }
 
+        int population = 0;
         int maxPop = 0;
         int ans = 1950;
 
         for (int i = 0; i <= 100; i++) {
-            if (population[i] > maxPop) {
-                maxPop = population[i];
+            population += diff[i];
+
+            if (population > maxPop) {
+                maxPop = population;
                 ans = 1950 + i;
             }
         }
